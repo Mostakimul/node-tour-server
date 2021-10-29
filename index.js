a20ceef9d64cce58cfd9afcf998626e55a5cc8b4
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-29 12:57:39 +0200

    initial database test

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+// DB user and password
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+// Mongo config
+const uri = `mongodb+srv://${user}:${password}@sandbox.5jrgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// main run fuction
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('travelDB');
+    const serviceCollection = database.collection('services');
+    console.log('Database Connected!!!');
+  } finally {
+    // await client.close();
+  }
+}
+
+// Running the run function
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node js server!');
+});
+
+app.listen(port, () => {
+  console.log('Listening to port', port);
+});
